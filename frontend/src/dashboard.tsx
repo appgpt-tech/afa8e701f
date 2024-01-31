d808a53fe50de0fbf572b325a511c47ea1df8747
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 21:10:01 +0200

    created on Wed Jan 31 2024 19:09:49 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "BookStore" />
+      <CardContent> An application to manage books, authors, and genres. < /CardContent>
+      </Card>
+    );
