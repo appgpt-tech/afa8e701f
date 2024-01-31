1e81138e59d0a50a799f9a505f0b374e059e9994
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 21:10:07 +0200

    created on Wed Jan 31 2024 19:09:49 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/Books.tsx
@@ -0,0 +1,93 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const BooksTitle = () => {
+  const record = useRecordContext();
+  return <span>Books {record ? `"${ record.Title }"` : ""}</span>;
+};
+
+export const BooksList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="Title" />
+<TextField source="Author" />
+<TextField source="Genre" />
+<TextField source="BookCover" />
+<NumberField source="id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const BooksEdit = () => (
+                    <Edit title={<BooksTitle />}>
+                      <SimpleForm>
+                          <TextInput source="Title"   />
+<TextInput source="Author"   />
+<TextInput source="Genre"   />
+<TextInput source="BookCover"   />
+<NumberInput source="id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const BooksCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="Title"   />
+<TextInput source="Author"   />
+<TextInput source="Genre"   />
+<TextInput source="BookCover"   />
+<NumberInput source="id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+
+    ];
+
+
