411364abbba7eab963c25cf9ed23d8ae23504295
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 21:09:58 +0200

    created on Wed Jan 31 2024 19:09:49 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,106 @@
+
+// in src/App.tsx
+import { Admin, Resource, CustomRoutes } from "react-admin";
+import postgrestRestProvider from "@promitheus/ra-data-postgrest";
+import fakeDataProvider from "ra-data-fakerest";
+import { Dashboard } from "./dashboard";
+import { authProvider, apInitialize } from "./authProvider";
+import { i18nProvider } from "./i18nProvider";
+import LoginPage, { Login } from "./Login";
+import data from "./data";
+import { BooksList, BooksCreate, BooksEdit} from "./resources/Books";
+import { AuthorsList, AuthorsCreate, AuthorsEdit} from "./resources/Authors";
+import { GenresList, GenresCreate, GenresEdit} from "./resources/Genres";
+import BooksIcon from "@mui/icons-material/Book";
+import AuthorsIcon from "@mui/icons-material/Person";
+import GenresIcon from "@mui/icons-material/Category"; 
+// SUPERTOKENS
+import React from "react";
+import SuperTokens, {
+  SuperTokensWrapper,
+  getSuperTokensRoutesForReactRouterDom,
+} from "supertokens-auth-react";
+import ThirdPartyPasswordless from "supertokens-auth-react/recipe/thirdpartypasswordless";
+import Session from "supertokens-auth-react/recipe/session";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import * as reactRouterDom from "react-router-dom";
+let sessionFn = Session.init();
+SuperTokens.init({
+  appInfo: {
+    appName: import.meta.env.VITE_SUPERTOKENS_APPNAME,
+    apiDomain: import.meta.env.VITE_BACKEND_DOMAIN,
+    websiteDomain: import.meta.env.VITE_SUPERTOKENS_WEBSITEDOMAIN,
+    apiBasePath: import.meta.env.VITE_BACKEND_APIPATH + "/auth",
+    websiteBasePath: import.meta.env.VITE_SUPERTOKENS_WEBSITEBASEPATH,
+  },
+  recipeList: [
+    ThirdPartyPasswordless.init({
+      contactMethod: "EMAIL",
+      signInUpFeature: {
+        providers: [
+          ThirdPartyPasswordless.Github.init(),
+          //ThirdPartyPasswordless.Google.init(),
+          //ThirdPartyPasswordless.Facebook.init(),
+          //ThirdPartyPasswordless.Apple.init(),
+        ],
+      },
+    }),
+    sessionFn,
+  ],
+});
+apInitialize(Session);
+// END SUPERTOKENS
+let dataProvider: any;
+if (import.meta.env.VITE_USE_BACKEND_DATA === "true") {
+  dataProvider = postgrestRestProvider(
+    import.meta.env.VITE_BACKEND_DOMAIN +
+      import.meta.env.VITE_BACKEND_APIPATH +
+      "/proxy"
+  );
+} else {
+  dataProvider = fakeDataProvider(data.defaultData);
+}
+
+const App = () => (
+  <SuperTokensWrapper>
+    <BrowserRouter basename="/afa8e701f">
+      <Admin
+        authProvider={
+          import.meta.env.VITE_ENVIRONMENT != "DEV" ? authProvider : undefined
+        }
+        requireAuth
+        loginPage={LoginPage}
+        dataProvider={dataProvider}
+        i18nProvider={i18nProvider}
+        dashboard={Dashboard}
+        
+      >
+    <Resource name="Books" options={{label:"books"}} 
+list={BooksList}
+create={BooksCreate}
+edit={BooksEdit}
+recordRepresentation="Title"
+icon={BooksIcon}/>
+<Resource name="Authors" options={{label:"authors"}} 
+list={AuthorsList}
+create={AuthorsCreate}
+edit={AuthorsEdit}
+recordRepresentation="Name"
+icon={AuthorsIcon}/>
+<Resource name="Genres" options={{label:"genres"}} 
+list={GenresList}
+create={GenresCreate}
+edit={GenresEdit}
+recordRepresentation="Category"
+icon={GenresIcon}/>
+    <CustomRoutes noLayout>
+      {/*This renders the login UI on the /auth route*/}
+      {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+      {/*Your app routes*/}
+    </CustomRoutes>
+  </Admin>
+  </BrowserRouter>
+  </SuperTokensWrapper>
+);
+
+export default App;
