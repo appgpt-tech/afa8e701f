f1772c743b4968d2b04cc916e078cfa66990bd21
11ai <124818466+11ai@users.noreply.github.com>
2024-02-05 22:33:21 +0200

    edited on Mon Feb 05 2024 20:33:08 GMT+0000 (Coordinated Universal Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{}};
+let data: any = {"defaultData":{"Users":[{"userId":"1","name":"John Doe","email":"johndoe@example.com","password":"password123","role":"admin"},{"userId":"2","name":"Jane Doe","email":"janedoe@example.com","password":"password123","role":"user"},{"userId":"3","name":"Bob Smith","email":"bobsmith@example.com","password":"password123","role":"user"}]}};
 export default data;
