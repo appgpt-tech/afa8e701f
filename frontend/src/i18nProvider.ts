c371921406230b24053581b59e0b5e706e50acaa
11ai <124818466+11ai@users.noreply.github.com>
2024-02-05 22:25:10 +0200

    edited on Mon Feb 05 2024 20:24:56 GMT+0000 (Coordinated Universal Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -4,8 +4,8 @@
     import enOriginal from 'ra-language-english';
 import frOriginal from 'ra-language-french';
 
-    const enResources = { resources: {"Users":{"name":"Users","fields":{"userId":"User ID","email":"Email","name":"Name","id":"id"}},"Cards":{"name":"Cards","fields":{"cardId":"Card ID","setId":"Set ID","cardName":"Card Name","serial":"Serial","type":"Type","rarity":"Rarity","condition":"Condition","imageUrl":"Image URL","id":"id"}},"Sets":{"name":"Sets","fields":{"setId":"Set ID","setName":"Set Name","releaseDate":"Release Date","totalCards":"Total Cards","id":"id"}},"Inventory":{"name":"Inventory","fields":{"userId":"User ID","cardId":"Card ID","recordedDate":"Recorded Date","id":"id"}},"Wishlist":{"name":"Wishlist","fields":{"userId":"User ID","cardId":"Card ID","insertedDate":"Inserted Date","id":"id"}}}};
-const frResources = { resources: {"Users":{"name":"Utilisateurs","fields":{"userId":"ID d'utilisateur","email":"Email","name":"Nom","id":"id"}},"Cards":{"name":"Cartes","fields":{"cardId":"ID de carte","setId":"ID de l'ensemble","cardName":"Nom de la carte","serial":"Série","type":"Type","rarity":"Rareté","condition":"Condition","imageUrl":"URL de l'image","id":"id"}},"Sets":{"name":"Ensembles","fields":{"setId":"ID de l'ensemble","setName":"Nom de l'ensemble","releaseDate":"Date de sortie","totalCards":"Total des cartes","id":"id"}},"Inventory":{"name":"Inventaire","fields":{"userId":"ID d'utilisateur","cardId":"ID de carte","recordedDate":"Date enregistrée","id":"id"}},"Wishlist":{"name":"Liste de souhaits","fields":{"userId":"ID d'utilisateur","cardId":"ID de carte","insertedDate":"Date insérée","id":"id"}}}};
+    const enResources = { resources: {}};
+const frResources = { resources: {}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
