9cf4a989034c07f8a89274ee3e26d26e28f02845
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 20:50:37 +0200

    edited on Wed Feb 07 2024 18:50:27 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "FitPulse" />
-      <CardContent> My app is a Health and Fitness app.This app caters to the growing interest in health and wellness people nowdays have. < /CardContent>
+      <CardHeader title= "PokeVault" />
+      <CardContent> My app is a web app to keep a Pokémon collectible cards inventory which involves blending themes of collection, organization, and the vibrant world of Pokémon. < /CardContent>
       </Card>
     );
