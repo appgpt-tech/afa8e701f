1fe62b12708bec3f93f97b2768a3f10a13fd97f7
11ai <124818466+11ai@users.noreply.github.com>
2024-02-11 20:57:15 +0200

    created on Sun Feb 11 2024 18:56:49 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/Vendors.tsx
@@ -0,0 +1,104 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const VendorsTitle = () => {
+  const record = useRecordContext();
+  return <span>Vendors {record ? `"${ record.vendorId }"` : ""}</span>;
+};
+
+export const VendorsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <NumberField source="vendorId" />
+<TextField source="companyName" />
+<TextField source="contactName" />
+<EmailField source="email" />
+<ReadOnlyPasswordField source="password" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const VendorsEdit = () => (
+                    <Edit title={<VendorsTitle />}>
+                      <SimpleForm>
+                          <NumberInput source="vendorId"   />
+<TextInput source="companyName"   />
+<TextInput source="contactName"   />
+<TextInput source="email"   />
+<PasswordInput source="password"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const VendorsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <NumberInput source="vendorId"   />
+<TextInput source="companyName"   />
+<TextInput source="contactName"   />
+<TextInput source="email"   />
+<PasswordInput source="password"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+,
+
+    ];
+
+
