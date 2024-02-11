dd40adc6b88b18c48f86926f3640657b2e4afd60
11ai <124818466+11ai@users.noreply.github.com>
2024-02-11 20:57:13 +0200

    created on Sun Feb 11 2024 18:56:49 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/Products.tsx
@@ -0,0 +1,128 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const ProductsTitle = () => {
+  const record = useRecordContext();
+  return <span>Products {record ? `"${ record.productId }"` : ""}</span>;
+};
+
+export const ProductsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <NumberField source="productId" />
+<ReferenceField source="vendorId" reference="Vendors"  />
+<TextField source="name" />
+<NumberField source="price" />
+<NumberField source="weight" />
+
+<ImageField source="thumbnail" />
+<ImageField source="image" />
+<ReferenceField source="category" reference="ProductCategories"  />
+<DateField source="createDate" />
+<NumberField source="stock" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const ProductsEdit = () => (
+                    <Edit title={<ProductsTitle />}>
+                      <SimpleForm>
+                          <NumberInput source="productId"   />
+<ReferenceInput source="vendorId"  reference="Vendors"   />
+<TextInput source="name"   />
+<NumberInput source="price"   />
+<NumberInput source="weight"   />
+<TextInput source="description"   />
+<ImageInput source="thumbnail"   />
+<ImageInput source="image"   />
+<ReferenceInput source="category"  reference="ProductCategories"   />
+<DateInput source="createDate"   />
+<NumberInput source="stock"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const ProductsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <NumberInput source="productId"   />
+<ReferenceInput source="vendorId"  reference="Vendors"   />
+<TextInput source="name"   />
+<NumberInput source="price"   />
+<NumberInput source="weight"   />
+<TextInput source="description"   />
+<ImageInput source="thumbnail"   />
+<ImageInput source="image"   />
+<ReferenceInput source="category"  reference="ProductCategories"   />
+<DateInput source="createDate"   />
+<NumberInput source="stock"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+<ReferenceInput source="vendorId" label="vendorId" reference="Vendors"   alwaysOn/>,
+,
+,
+,
+,
+,
+,
+<ReferenceInput source="category" label="category" reference="ProductCategories"   alwaysOn/>,
+,
+,
+
+    ];
+
+
