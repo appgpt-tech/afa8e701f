849f235b3e889dc5591253c2c4c46f7db09590f8
11ai <124818466+11ai@users.noreply.github.com>
2024-02-11 20:57:34 +0200

    edited on Sun Feb 11 2024 18:56:49 GMT+0000 (Coordinated Universal Time)

--- backend/src/db/Inventory.entity.ts
+++ backend/src/db/Inventory.entity.ts
@@ -6,14 +6,14 @@ export class InventoryEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({nullable: true})
-userId: number;
+  @Column("integer",{nullable: true})
+productId: number;
 
-@Column({nullable: true})
-cardId: number;
+@Column("integer",{nullable: true})
+quantity: number;
 
-@Column({nullable: true})
-recordedDate: Date;
+@Column("integer",{nullable: true})
+lowStockThreshold: number;
 
 
 }
