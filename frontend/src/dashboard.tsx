749205e723d751111f983ffd9727af72a927ee06
11ai <124818466+11ai@users.noreply.github.com>
2024-02-12 22:38:51 +0200

    edited on Mon Feb 12 2024 20:38:34 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "ShopSphere" />
-      <CardContent> An ecommerce site to provide a seamless shopping experience for users worldwide < /CardContent>
+      <CardHeader title= "PokeVault" />
+      <CardContent> A web app to keep a Pokémon collectible cards inventory which involves blending themes of collection, organization, and the vibrant world of Pokémon. < /CardContent>
       </Card>
     );
