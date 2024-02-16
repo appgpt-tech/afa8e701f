e3094387a9b6df2f61d18a5b1863f43eec434315
11ai <124818466+11ai@users.noreply.github.com>
2024-02-16 07:31:05 +0200

    edited on Fri Feb 16 2024 05:30:58 GMT+0000 (Coordinated Universal Time)

--- frontend/src/resources/SupportTickets.tsx
+++ frontend/src/resources/SupportTickets.tsx
@@ -32,6 +32,7 @@ import {
   PasswordInput
 } from "react-admin";
 import { useRecordContext } from "react-admin";
+import { Grid } from '@mui/material';
 const ReadOnlyPasswordField = ({ record, source }) => {
 
   // You can customize the way you display the password here, e.g., mask it with asterisks
@@ -51,7 +52,7 @@ const ListActions = () => (
 );
 const SupportTicketsTitle = () => {
   const record = useRecordContext();
-  return <span>SupportTickets {record ? `"${ record.ticketId }"` : ""}</span>;
+  return <span>SupportTickets {record ? `"${ record.description }"` : ""}</span>;
 };
 
 export const SupportTicketsList = () => (
@@ -72,13 +73,22 @@ export const SupportTicketsList = () => (
 export const SupportTicketsEdit = () => (
                     <Edit title={<SupportTicketsTitle />}>
                       <SimpleForm>
-                          <NumberInput source="ticketId"   />
-<ReferenceInput source="userId"  reference="Users"   />
-<ReferenceInput source="customerId"  reference="Customers"   />
-<TextInput source="description"   />
-<TextInput source="status"   />
-<DateInput source="creationDate"   />
-<DateInput source="resolutionDate"   />
+                        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 5 }}>
+                          <Grid item xs={4}>
+<NumberInput source="ticketId"   /></Grid>
+<Grid item xs={4}>
+<ReferenceInput source="userId"  reference="Users"   /></Grid>
+<Grid item xs={4}>
+<ReferenceInput source="customerId"  reference="Customers"   /></Grid>
+<Grid item xs={4}>
+<TextInput source="description"   /></Grid>
+<Grid item xs={4}>
+<TextInput source="status"   /></Grid>
+<Grid item xs={4}>
+<DateInput source="creationDate"   /></Grid>
+<Grid item xs={4}>
+<DateInput source="resolutionDate"   /></Grid>
+                        </Grid>
                       </SimpleForm>
                     </Edit>
                   );
@@ -86,13 +96,22 @@ export const SupportTicketsEdit = () => (
 export const SupportTicketsCreate = () => (
                                   <Create>
                                     <SimpleForm>
-                                        <NumberInput source="ticketId"   />
-<ReferenceInput source="userId"  reference="Users"   />
-<ReferenceInput source="customerId"  reference="Customers"   />
-<TextInput source="description"   />
-<TextInput source="status"   />
-<DateInput source="creationDate"   />
-<DateInput source="resolutionDate"   />
+                                      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 5 }}>
+                                        <Grid item xs={4}>
+<NumberInput source="ticketId"   /></Grid>
+<Grid item xs={4}>
+<ReferenceInput source="userId"  reference="Users"   /></Grid>
+<Grid item xs={4}>
+<ReferenceInput source="customerId"  reference="Customers"   /></Grid>
+<Grid item xs={4}>
+<TextInput source="description"   /></Grid>
+<Grid item xs={4}>
+<TextInput source="status"   /></Grid>
+<Grid item xs={4}>
+<DateInput source="creationDate"   /></Grid>
+<Grid item xs={4}>
+<DateInput source="resolutionDate"   /></Grid>
+                                      </Grid>
                                     </SimpleForm>
                                   </Create>
                                 );
