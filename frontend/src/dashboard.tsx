cfda295bb579a89aba14c1a744e0fd64c07b9fb0
11ai <124818466+11ai@users.noreply.github.com>
2024-03-28 18:40:24 +0200

    edited on Thu Mar 28 2024 16:40:12 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -3,9 +3,9 @@
  import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title="FitPulse" />
+      <CardHeader title="libby" />
       <CardContent> 
-      Health and Fitness app.This app caters to the growing interest in health and wellness people nowdays have.
+      library management system
       <p>Built with{' '}
       <a
         href="https://www.appgpt.tech"
